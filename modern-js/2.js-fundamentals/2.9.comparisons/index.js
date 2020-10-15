6b19b56091f4074b8bab9547ea6e054ab47f58d6
narcello <marcellovcs@gmail.com>
2020-10-15 08:27:25 -0300

    add comparison task

---
+++ modern-js/2.js-fundamentals/2.9.comparisons/index.js
@@ -0,0 +1,25 @@
+/* 
+What will be the result for these expressions?
+
+5 > 4
+r: true
+
+"apple" > "pineapple"
+r: false
+
+"2" > "12"
+r: true
+
+undefined == null
+r: true
+
+undefined === null
+r: false
+
+null == "\n0\n"
+r: false
+
+null === +"\n0\n"
+r: false
+
+*/
