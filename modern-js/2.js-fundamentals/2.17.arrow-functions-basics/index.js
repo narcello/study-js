90bde6ae0a15415ca1f3dc7a0307464d3b32bdd5
narcello <marcellovcs@gmail.com>
2020-10-28 08:34:20 -0300

    add arrow functions tasks

---
+++ modern-js/2.js-fundamentals/2.17.arrow-functions-basics/index.js
@@ -0,0 +1,18 @@
+/* 
+Rewrite with arrow functions
+Replace Function Expressions with arrow functions in the code below:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+r:
+let ask = (question, yes, no) => confirm(question) ? yes() : no()
+*/
