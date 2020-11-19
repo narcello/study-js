6c661a51628bdd4972ab5a0a22b114cf1e519512
narcello <marcellovcs@gmail.com>
2020-11-19 08:25:40 -0300

    refactor objects folder and add new data types folder

---
+++ modern-js/5.js-data-types/5.1.methods-of-primitives/index.js
@@ -0,0 +1,12 @@
+/* 
+Consider the following code:
+
+let str = "Hello";
+
+str.test = 5;
+
+alert(str.test);
+How do you think, will it work? What will be shown? 
+
+r: In use strict mode get in error, otherwise get undefined.
+*/
